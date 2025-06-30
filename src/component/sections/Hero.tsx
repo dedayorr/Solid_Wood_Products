da9d6322f4ab5aedb9b815404b1bce7354d3e4d6
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2025-06-30 16:05:52 +0100

    heroSection

--- src/component/sections/Hero.tsx
+++ src/component/sections/Hero.tsx
@@ -1,4 +1,3 @@
-
 import Hero_Img from "../../assets/images/hero_image.png";
 function Hero() {
   return (
@@ -8,9 +7,14 @@ function Hero() {
         src={Hero_Img}
         alt="woods"
       />
-      <div className="relative z-30 bg-[#1E0C06] w-[80%] h-[280px] rounded-tl-[14px] rounded-bl-[14px] float-right">
-        <p className="text-white">Solid wood products</p>
+      <div className="relative z-30 p-[5%] bg-[#1E0C06] w-[80%] h-[280px] rounded-tl-[14px] rounded-bl-[14px] float-right">
+        <h1 className="text-white font- text-[30px] font-medium leading-[108%] tracking-wider">
+          SOLID WOOD PRODUCTS
+        </h1>
+        <p className="text-[#FFDBBB] text-[15px]  w-[185px]">Oak, beech, ash from 1700 CZK per m3</p>
+        <button className="text-white bg-[#728BAD] w-[86px] h-[21px]">Order</button>
       </div>
+      
     </div>
   );
 }
